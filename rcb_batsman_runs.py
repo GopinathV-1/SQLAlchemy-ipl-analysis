7bc4a3db5b326c150993c76105b7a8b811a3f15a
Gopinath V <gopinath.v.16@mountblue.tech>
2021-06-03 12:04:50 +0530

    improved readability

--- rcb_batsman_runs.py
+++ rcb_batsman_runs.py
@@ -1,4 +1,4 @@
-import total_run
+from total_run import session, func, json, create_table_from_csv
 
 
 def create_json(Deliveries):
@@ -6,12 +6,12 @@ def create_json(Deliveries):
         in the table'''
 
     # Query to pick top ten run scored RCB batsmans
-    top_rcb_batsman = (total_run.session.query(
+    top_rcb_batsman = (session.query(
                         Deliveries.batsman,
-                        total_run.func.sum(Deliveries.total_runs)).filter(
+                        func.sum(Deliveries.total_runs)).filter(
                         Deliveries.batting_team == 'RCB').group_by(
                         Deliveries.batsman).order_by(
-                        total_run.func.sum(
+                        func.sum(
                             Deliveries.total_runs).desc()).limit(10).all())
 
     top_rcb_batsman = [list(row) for row in top_rcb_batsman]
@@ -19,10 +19,10 @@ def create_json(Deliveries):
     data['rcb_batsman_runs'] = top_rcb_batsman
 
     with open('rcb_batsman_runs.json', 'w') as file:
-        total_run.json.dump(data, file, indent=4)
+        json.dump(data, file, indent=4)
 
 
 if __name__ == "__main__":
 
-    Deliveries = total_run.create_table_from_csv()
+    Deliveries = create_table_from_csv()
     create_json(Deliveries)
